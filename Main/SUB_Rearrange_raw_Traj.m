d67491c9c6a8a82a526c811a407e1e284c9d47a4
LDJ <zinith7@naver.com>
2025-08-20 18:09:50 +0900

    feat: 곡면 금형 데이터 전처리 코드 추가

---
+++ Main/SUB_Rearrange_raw_Traj.m
@@ -0,0 +1,28 @@
+% 파이썬 코드(extract_z_vector)를 통해 STL로부터 추출한 초기 금형 형상
+% 데이터(indexed_path_data_Y_axis)를 재정렬 및 전처리하는 코드
+
+clear;
+close all;
+clc;
+
+Initialization;
+
+%% 경로 데이터 불러오기
+pre_traj = readmatrix('indexed_path_data_Y_axis.csv');
+
+%% y축을 기준으로 오름차순 정렬
+[y_sort, idx_sort] = sort(pre_traj(:,3));
+traj = pre_traj(idx_sort,:);
+
+%% 중복되는 좌표 제거
+% y축 위치 좌표를 이용해서 제거
+new_traj = [];
+for i = 1:length(traj(:,3))
+    if mod(i,2) == 1
+        new_traj = [new_traj; traj(i,2:7)];
+    end
+end
+
+%% 시각화를 통한 확인
+figure;
+plot(new_traj(:,2))
